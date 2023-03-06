59e7c29e49c93177de2adb68a30ce25633780ca2
uomma <marcelloleone975@gmail.com>
2023-03-06 16:55:49 +0100

    array random numbers

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 'use strict'
+
+
+ function randomNumbers(min, max){
+   let numeri = [];
+   while (numeri.length < 5){
+    const i = Math.floor(Math.random() * (max - min + 1) + min);
+
+   if (!numeri.includes(i)){
+    numeri.push(i);
+   }
+}
+   
+return numeri;
+ }
+const iMax=100;
+const iMin=1;
+const numeri = randomNumbers(iMax,iMin)
+ console.log(numeri)
+ 
